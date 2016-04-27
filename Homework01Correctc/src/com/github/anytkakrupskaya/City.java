55a5e5879034da79c569605d35aabdd5b16f8187
AnytkaKrupskaya <anetka.chiki.bum@gmail.com>
2016-04-27 08:55:46 +0300

    Update City.java

--- Homework01Correctc/src/com/github/anytkakrupskaya/City.java
+++ Homework01Correctc/src/com/github/anytkakrupskaya/City.java
@@ -4,38 +4,53 @@ package com.github.anytkakrupskaya;
 * Created by Аня on 19.03.2016. 
 */ 
 public class City { 
-protected String name ; 
-protected int area ; 
-protected String mayor ; 
-protected int numberOfResidents ; 
-protected int coordinates ; 
+    protected String name; 
+    protected int area; 
+    protected String mayor ; 
+    protected int numberOfResidents; 
+    protected double latitude;
+    protected double longitude;
+    
 
-public void setName(String name) { 
-this.name = name; 
-} 
-public String getName() {return name;} 
+    public void setName(String name) { 
+        this.name = name; 
+    } 
+    public String getName() {return name;} 
 
-public void setArea (int area) { 
-this.area = area; 
-} 
-public int getArea() {return area;} 
+    public void setArea (int area) { 
+        this.area = area; 
+    } 
+    public int getArea() {return area;} 
 
-public void setMayor (String mayor) { 
-this.mayor = mayor; 
-} 
-public String getMayor() {return mayor;} 
+    public void setMayor (String mayor) { 
+        this.mayor = mayor; 
+    } 
+    public String getMayor() {return mayor;} 
 
-public void setNumberOfResidents (int numberOfResidents) { 
-this.numberOfResidents = numberOfResidents; 
-} 
-public int getNumberOfResidents() {return numberOfResidents;} 
+    public void setNumberOfResidents (int numberOfResidents) { 
+        this.numberOfResidents = numberOfResidents; 
+    } 
+    public int getNumberOfResidents() {return numberOfResidents;} 
 
-public void setCoordinates (int coordinates) { 
-this. coordinates = coordinates; 
-} 
-
-public String toString() { 
-return "About city \nName: " + this.name + "\n area: " + this.area + "\n mayor: " + this.mayor + "\n number of residents: " + this.numberOfResidents + "\n coordinates: " + this.coordinates; 
+    public void setLatitude (double latitude) { 
+        this.latitude; 
+    } 
+    
+     public void setLongitude (double longitude) { 
+        this.longitude = longitude; 
+    } 
+    
+    
+   public City (String name, int area, String mayor, int numberOfResidents, double latitude, double longitude ) { 
+    this.name = name; 
+    this.area=area; 
+    this.mayor=mayor ; 
+    this.numberOfResidents=numberOfResidents; 
+    this.latitude=atitude ; 
+    this.longitude = longitude;
+    
+    public String toString() { 
+    return "About city \nName: " + this.name + "\n area: " + this.area + "\n mayor: " + this.mayor + "\n number of residents: " + this.numberOfResidents + "\n latitude: " + this.latitude + "\n longitudes: " + this.longitude; 
 
 } 
 
