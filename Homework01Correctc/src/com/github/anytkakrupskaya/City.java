49c2009dd462d8d4cfe7f18256dac2c02b586d76
AnytkaKrupskaya <anetka.chiki.bum@gmail.com>
2016-04-27 09:13:29 +0300

    Update City.java

--- Homework01Correctc/src/com/github/anytkakrupskaya/City.java
+++ Homework01Correctc/src/com/github/anytkakrupskaya/City.java
@@ -1,57 +1,22 @@
-package com.github.anytkakrupskaya; 
+package com.github.anytkakrupskaya;
 
-/** 
-* Created by Аня on 19.03.2016. 
-*/ 
-public class City { 
-    protected String name; 
-    protected int area; 
-    protected String mayor ; 
-    protected int numberOfResidents; 
-    protected double latitude;
-    protected double longitude;
-    
+public class Main {
+    public static void main(String[] args) {
+        City city = new City ();
 
-    public void setName(String name) { 
-        this.name = name; 
-    } 
-    public String getName() {return name;} 
+        city.setName("London");
+        city.getName();
+        city.setArea(8);
+        city.getArea();
+        city.setMayor("Johnson");
+        city.getMayor();
+        city.setNumberOfResidents(4589658);
+        city.getNumberOfResidents();
+        city.setLatitude(7656);
+        city.getLatitude();
+        city.setLongitude(93775);
+        city.getLongitude();
 
-    public void setArea (int area) { 
-        this.area = area; 
-    } 
-    public int getArea() {return area;} 
-
-    public void setMayor (String mayor) { 
-        this.mayor = mayor; 
-    } 
-    public String getMayor() {return mayor;} 
-
-    public void setNumberOfResidents (int numberOfResidents) { 
-        this.numberOfResidents = numberOfResidents; 
-    } 
-    public int getNumberOfResidents() {return numberOfResidents;} 
-
-    public void setLatitude (double latitude) { 
-        this.latitude; 
-    } 
-    
-     public void setLongitude (double longitude) { 
-        this.longitude = longitude; 
-    } 
-    
-    
-   public City (String name, int area, String mayor, int numberOfResidents, double latitude, double longitude ) { 
-        this.name = name; 
-        this.area=area; 
-        this.mayor=mayor ; 
-        this.numberOfResidents=numberOfResidents; 
-        this.latitude=atitude ; 
-        this.longitude = longitude;
-   }
-   
-    public String toString() { 
-        return "About city \nName: " + this.name + "\n area: " + this.area + "\n mayor: " + this.mayor + "\n number of residents: " + this.numberOfResidents + "\n latitude: " + this.latitude + "\n longitudes: " + this.longitude; 
-    } 
-
+        System.out.println(city.toString());
+    }
 }
